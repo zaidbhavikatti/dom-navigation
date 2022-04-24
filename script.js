2d4fd7f21a620199204290f7b42d4809678d1c82
zaidbhavikatti <98631203+zaidbhavikatti@users.noreply.github.com>
2022-04-24 12:57:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var main=document.getElementById("parent");
+console.log(main.childNodes);
+
+var main=document.getElementById("parent");
+console.log(main.children);
+
+var main=document.getElementById("parent");
+console.log(main.firstChild);
+
+var main=document.getElementById("parent");
+console.log(main.firstElementChild);
+
+var main=document.getElementById("parent");
+console.log(main.lastChild);
+
+var main=document.getElementById("parent");
+console.log(main.lastElementChild);
+
+var home=document.getElementById("home");
+console.log(home.previousSibling);
+
+var home=document.getElementById("home");
+console.log(home.previousElementSibling);
+
+var spany=document.getElementById("spany");
+console.log(spany.nextSibling);
+
+var spany=document.getElementById("spany");
+console.log(spany.nextElementSibling);
